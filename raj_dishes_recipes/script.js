6018d7c9caa3b7999cb9f6267f668dcd84fd3a14
vaibhav-rana <59756505+vaibhav-rana@users.noreply.github.com>
2020-01-11 16:15:51 +0530

    Add files via upload

---
+++ raj_dishes_recipes/script.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+        document.getElementById('button1').addEventListener('click',
+            function(){
+                document.querySelector('.bg-modal').style.display='flex';
+        });
+        document.querySelector('.close').addEventListener('click',
+            function(){
+                document.querySelector('.bg-modal').style.display='none';
+        });
+</script>
